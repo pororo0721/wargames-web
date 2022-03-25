d27d02c280f58eaa36b94ba09da54010ca109075
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-03-25 17:07:34 +0000

    fixed footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 
 import { React } from 'react';
+import "./App.css";
 // Routing
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 // Components
-import NavbarComp from './components/NavbarComp';
-import Footer from './components/Footer';
-// Routes
+import NavbarComp from './components/Navbar/NavbarComp';
+import Footer from './components/Footer/Footer';
+
+// Pages
+import Home from './pages/Home/Home';
+
 
 
 
 
 function App() {
   return (
+    <div className="page-container">
+      <div className= "content-wrap">
+      
     <Router>
     <NavbarComp />
     <Switch>
-
+      <Route exact path="/" component={Home} />
     </Switch>
-    <Footer/>
     </Router>
-  )
+
+    </div>
+    <Footer />
+    </div>
+    
+  );
 }
 
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <image src="https://scontent-lcy1-1.xx.fbcdn.net/v/t39.30808-6/272720559_111063764815122_7650649713770568467_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=d37NSgP4R3YAX9IJuVa&_nc_ht=scontent-lcy1-1.xx&oh=00_AT-nnHgKsDmJO-WMmPciZaiBra4iLzoSgV4CcSVmIDIqRg&oe=624270E6" alt="Gloster Models" />
+        </div>
+    )
+}
+
+export default Home;
