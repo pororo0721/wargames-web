68c8e5e69cb10e8d8ce783428d45870b6b07a2d7
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-06-13 16:51:47 +0100

    Edit items img

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 
-
 import { Productscontainer, 
          ProductsH1, 
          ProductsWrapper, 
@@ -13,23 +12,23 @@ import { Productscontainer,
 const Products = () => {
   return (
     <Productscontainer id="products">
-        <ProductsH1>Our Products</ProductsH1>
+        <ProductsH1>Items For Sale</ProductsH1>
         <ProductsWrapper>
             <ProductsCard>
                 <ProductsIcon src="https://i.ebayimg.com/images/g/FOwAAOSwOvZglkXj/s-l500.jpg"/>
-                    <ProductsH2>Bolt Action</ProductsH2>
+                    <ProductsH2>BOLT ACTION</ProductsH2>
             </ProductsCard>
             <ProductsCard>
                 <ProductsIcon src="https://i.ebayimg.com/images/g/PpgAAOSwEcNgQffp/s-l500.jpg"/>
-                    <ProductsH2>Black Powder</ProductsH2>
+                    <ProductsH2>BLACK POWDER</ProductsH2>
             </ProductsCard>
             <ProductsCard>
-                <ProductsIcon src={Icon1}/>
-                    <ProductsH2>Pike & shotte</ProductsH2>
+                <ProductsIcon src="https://i.ebayimg.com/images/g/YF8AAOSwt1ph0ZL~/s-l500.gif"/>
+                    <ProductsH2>GRIPPING BEAST PLASTICS</ProductsH2>
             </ProductsCard>
             <ProductsCard>
-                <ProductsIcon src={Icon1}/>
-                    <ProductsH2>Hail Caesar</ProductsH2>
+                <ProductsIcon src="https://i.ebayimg.com/images/g/IUsAAOSwIblh0dV3/s-l500.jpg"/>
+                    <ProductsH2>PIKE & SHOTTE</ProductsH2>
             </ProductsCard>
             <ProductsCard>
                 <ProductsIcon src="https://i.ebayimg.com/images/g/zcEAAOSwNxNh0YCa/s-l500.jpg"/>
@@ -37,43 +36,43 @@ const Products = () => {
             </ProductsCard>
             <ProductsCard>
                 <ProductsIcon src="https://i.ebayimg.com/images/g/RkIAAOSw2hRh0w5F/s-l500.jpg"/>
-                    <ProductsH2>Project Z</ProductsH2>
+                    <ProductsH2>PROJECT Z</ProductsH2>
             </ProductsCard>
             <ProductsCard>
-                <ProductsIcon src={Icon1}/>
-                    <ProductsH2>Strontium Dog</ProductsH2>
+                <ProductsIcon src="https://i.ebayimg.com/images/g/AFYAAOSwmBNh0YOn/s-l500.jpg"/>
+                    <ProductsH2>HAIL CAESAR</ProductsH2>
             </ProductsCard>
             <ProductsCard>
-                <ProductsIcon src={Icon1}/>
-                    <ProductsH2>Judge Dread</ProductsH2>
+                <ProductsIcon src="https://i.ebayimg.com/images/g/K4wAAOSwIQdZKVOM/s-l500.jpg"/>
+                    <ProductsH2>DR WHO </ProductsH2>
             </ProductsCard>
                 <ProductsCard>
                     <ProductsIcon src="https://i.ebayimg.com/images/g/C2sAAOSwxCBiJOm9/s-l500.jpg"/>
-                        <ProductsH2>Warhammer 40,000</ProductsH2>
+                        <ProductsH2>WARHAMMER 40,000</ProductsH2>
                 </ProductsCard>
                 <ProductsCard>
-                    <ProductsIcon src={Icon2}/>
-                        <ProductsH2>Warhammer Age of sigmar</ProductsH2>  
+                    <ProductsIcon src="https://i.ebayimg.com/images/g/hpQAAOSwutBiGnj2/s-l500.jpg"/>
+                        <ProductsH2>WARHAMMER AGE OF SIGMAR</ProductsH2>  
                 </ProductsCard>
                 <ProductsCard>
                 <ProductsIcon src="https://i.ebayimg.com/thumbs/images/g/U9oAAOSwLalh0fvK/s-l225.jpg"/>
-                    <ProductsH2>Perry Miniatures</ProductsH2>
+                    <ProductsH2>PERRY MINIATURES</ProductsH2>
             </ProductsCard>
             <ProductsCard>
-                <ProductsIcon src={Icon1}/>
-                    <ProductsH2>Gripping Beast</ProductsH2>
+                <ProductsIcon src="https://i.ebayimg.com/images/g/WO8AAOSwj4Zihqiz/s-l500.jpg"/>
+                    <ProductsH2>RUBICON MODELS</ProductsH2>
             </ProductsCard>
             <ProductsCard>
-                <ProductsIcon src={Icon1}/>
-                    <ProductsH2>Deus Vult</ProductsH2>
+                <ProductsIcon src="https://i.ebayimg.com/images/g/fhwAAOSw8eZh0aAj/s-l500.jpg"/>
+                    <ProductsH2>DEUS VULT</ProductsH2>
             </ProductsCard>
             <ProductsCard>
-                <ProductsIcon src={Icon1}/>
-                    <ProductsH2>Rendera Accessories</ProductsH2>
+                <ProductsIcon src="https://i.ebayimg.com/images/g/GgYAAOSwcr9geJSD/s-l500.jpg"/>
+                    <ProductsH2>BEYOND THE GATES OF ANTARES</ProductsH2>
             </ProductsCard>
                 <ProductsCard>
-                    <ProductsIcon src={Icon3}/>
-                        <ProductsH2>Paints & Tools & Acessories</ProductsH2>
+                    <ProductsIcon src="https://scontent-lhr8-2.xx.fbcdn.net/v/t39.30808-6/274336934_125554943366004_8028215225043424416_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=RCxZIDM8LRgAX_su3lO&tn=JzTt1GBWkOjzJITh&_nc_ht=scontent-lhr8-2.xx&oh=00_AT8uH8kDqSzOI2_zOgr5JPZXZvFiBYCBPKNsJI7OysJzmw&oe=62ACD452"/>
+                        <ProductsH2>PAINT & TOOLS & ACESSORIES</ProductsH2>
             </ProductsCard>
         </ProductsWrapper>
 
